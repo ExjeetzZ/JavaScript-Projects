58fc81f715ddbb1017db31c973ebc09adb4578f4
ExjeetzZ <pushpgill2@gmail.com>
2025-06-11 11:39:36 -0700

    Basic JavaScript Projects Assignment
    *Module 1
    - String, Escape Character, Concatenation, Multiple Variables, Expressions

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Assignments/Module 1/String, Escape Character, Concatenation, Multiple Variables, Expressions/JS/main.js
@@ -0,0 +1,30 @@
+var A = "This is a string";
+// Variable A is defined as a string and will be displayed 
+var B = "This is another string";
+// Variable B is defined as another string and will not display in the alert 
+var C = "This is yet another string \"with a quote\" inside it, Exciting isn't it?";
+// Variable C is defined as yet another string with a quote inside it 
+window.alert(A);
+// The alert will display the value of variable A 
+document.write(C);
+// The document will write the value of variable C, which includes a quote and an apostrophe
+document.write("\Be who you are and say what you feel," 
++ "because those who mind don't matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+//This is an example of Concatenation
+
+var D = "This is Concatenated" + " String";
+document.write("<br>" + B);
+//This is an example of Concatenation String
+
+var Family ="The Smiths", Dad = "John", Mom = "Jane", Son = "Jack", Daughter = "Jill";
+document.write("<br>" + Family);
+document.write("<br>" + Dad);
+document.write("<br>" + Mom);
+document.write("<br>" + Son);
+document.write("<br>" + Daughter);
+document.write("<br>" + "The Smiths are a happy family."); 
+//This is an example of Multiple Variables
+
+document.write("<br>" + (3 + 3));
+//This is an example of Expressions
